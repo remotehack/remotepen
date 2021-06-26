f3a5652c2f8d15a3293561396e1309777df76c8a
Rumyra <rumyra@gmail.com>
2021-06-26 13:34:41 +0100

    using demo files

--- app.js
+++ app.js
@@ -2,6 +2,35 @@ const htmledit = document.querySelector('#html');
 const cssedit = document.querySelector('#css');
 const jsedit = document.querySelector('#js');
 
+
+// so here we need to get file contents and add it to the textarea, rather than use what is loaded
+
+fetch('./demo/index.html')
+  .then(response => response.text())
+  .then(data => {
+    htmledit.value = data;
+  });
+
+fetch('./demo/style.css')
+  .then(response => response.text())
+  .then(data => {
+    cssedit.value = data;
+  });
+
+fetch('./demo/script.js')
+  .then(response => response.text())
+  .then(data => {
+    jsedit.value = data;
+  });
+
+
+// fetch into file object
+// async function getText(fileURL) {
+
+// }
+
+
+
 const preview = document.querySelector('#preview').contentWindow.document;
 
 
--- demo/script.js
+++ demo/script.js
@@ -1,1 +1,2 @@
 document.querySelector('h1');
+console.log('This is some JS from the demo')
